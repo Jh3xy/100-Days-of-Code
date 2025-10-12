14b39bfc7b8a0d2c7a7267eab96b97c5ba93193c
Jh3xy <jhxydev.me@gmail.com>
2025-10-12 07:48:06 -0400

    add navbar functionality on mobile

--- Nav and Header | Flex and Media Queries - DAY 19/script.js
+++ Nav and Header | Flex and Media Queries - DAY 19/script.js
@@ -1,1 +1,20 @@
-console.log('Hello');
+
+// Navbar Functionality on Mobile
+
+//Grab HTML elements 
+let dropDown = document.querySelector('.dropdown_menu')
+let navLinks = document.querySelector('.nav_links')
+//console.log(dropDown.classList)
+
+
+//Add Click Event Listener to dropDown
+
+dropDown.addEventListener("click", ()=>{
+ 
+ //Toggle class of 'toggle' on navLinks
+ navLinks.classList.toggle('toggle')
+ console.log(navLinks)
+})
+
+
+
